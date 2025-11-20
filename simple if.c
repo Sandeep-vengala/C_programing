62bd6065aab9af360c1f27565aa8a6dbf36820cc
Sandeep-vengala <sandeepvengala2580@gmail.com>
2025-11-20 15:18:23 +0530

    Add files via upload

---
+++ simple if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+
+    printf("welcome to instagram\n");
+    int pwd=4593;
+    int upwd;
+    printf("enter the password\n");
+    scanf("%d",&upwd);
+    if(pwd==upwd)
+    {
+    printf("name:Sandeep\n");
+    printf("age:18\n");
+    }
+    printf("program ends");
+}
